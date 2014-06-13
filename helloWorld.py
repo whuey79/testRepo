1bb782186d0b93dba05ce80f90371d64ea4a9179
whuey79 <wdhuey@gmail.com>
2014-06-13 12:02:52 -0700

    fix conflicts

--- helloWorld.py
+++ helloWorld.py
@@ -5,12 +5,8 @@ import sys
 def main():
   print "Hello World!"
   print "Hello Group Github"
-<<<<<<< HEAD
-  
-  print "Hello 4th print"
-=======
   print "Hello Team"
->>>>>>> 1824d4a332c04846ba743d719fefa293e802bedd
+  print "Hello 4th print"
 
 if __name__ == '__main__':
   main()
